2aa5ad0744715cf475996f23d8f39180e93dfa9c
W1ct2 <renskove@gmail.com>
2025-11-13 10:34:49 +0300

    commit-70 (update vue config)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
       definitions[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false
